748df29d3c5b1f7ee195f92fb57295a68a98183c
sjcswank <sjcswank@gmail.com>
2019-04-27 17:43:59 -0500

    Updates

---
+++ precache-manifest.119fe0f7aa9607a25be31f3670318ce2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca8a1a64139c3bf060553720309ec65",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "878457d445b315fb51ff",
+    "url": "/react-calc/static/css/main.caec9e11.chunk.css"
+  },
+  {
+    "revision": "a4f5bf2d288a6f2e59f2",
+    "url": "/react-calc/static/js/2.c3c04ce8.chunk.js"
+  },
+  {
+    "revision": "878457d445b315fb51ff",
+    "url": "/react-calc/static/js/main.3d77ad71.chunk.js"
+  },
+  {
+    "revision": "48eedb427e7cd8bd6dd1",
+    "url": "/react-calc/static/js/runtime~main.0d723d41.js"
+  }
+]);
